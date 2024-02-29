5396dc8dde2ea539a713527f43d15b218fd2eb36
MoatazSalah306 <139802667+MoatazSalah306@users.noreply.github.com>
2024-02-29 05:23:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,31 @@
-const texts = document.querySelector( '.texts' );
-const startBtn = document.getElementById( 'startBtn' );
-const endBtn = document.getElementById( 'endBtn' );
+const texts = document.querySelector('.texts');
+const startBtn = document.getElementById('startBtn');
+const endBtn = document.getElementById('endBtn');
+
 
 // morse code for each letter
 var cipher = {
-  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
-  'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-  'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
-  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-  'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-  'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-  'Y': '-.--', 'Z': '--..'
+  'A': '.-',     'B': '-...',   'C': '-.-.',   'D': '-..',
+  'E': '.',      'F': '..-.',   'G': '--.',    'H': '....',
+  'I': '..',     'J': '.---',   'K': '-.-',    'L': '.-..',
+  'M': '--',     'N': '-.',     'O': '---',    'P': '.--.',
+  'Q': '--.-',   'R': '.-.',    'S': '...',    'T': '-',
+  'U': '..-',    'V': '...-',   'W': '.--',    'X': '-..-',
+  'Y': '-.--',   'Z': '--..'
 };
 
-var charVibrate = {
-  ".": 200,
-  "-": 500
-};
 
 // alphapets uppercase
-var uppercaseAlphabets = [ " " ];
-for ( var i = 65; i <= 90; i++ )
-{
-  uppercaseAlphabets.push( String.fromCharCode( i ) );
+var uppercaseAlphabets = [" "];
+for (var i = 65; i <= 90; i++) {
+    uppercaseAlphabets.push(String.fromCharCode(i));
 }
 
-const vibrateFun = ( char, index ) =>
-{
-  setTimeout( () =>
-  {
-    if ( char == "-" )
-    {
-      navigator.vibrate( charVibrate[ "-" ] );
-      console.log( charVibrate[ "-" ] );
-    }
-    else if ( char == "." )
-    {
-      navigator.vibrate( charVibrate[ "." ] );
-      console.log( charVibrate[ "." ] );
-    }
-  }, 700 * index );
-
-
-};
-
 // will contain the final morse code for the recognised text, and " " for the space between talking
-var answer = [];
+var answer=[];
 
 // the final text for the user will be here
-let finalResult = "";
+let finalResult = ""
 
 // recognition process
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -56,56 +33,57 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-let p = document.createElement( 'p' );
+let p = document.createElement('p');
 
-recognition.addEventListener( 'result', ( e ) =>
-{
-  texts.appendChild( p );
-  const text = Array.from( e.results )
-    .map( result => result[ 0 ] )
-    .map( result => result.transcript )
-    .join( '' );
+recognition.addEventListener('result', (e) => {
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
 
   p.innerText = text;
-  finalResult = text;
-} );
+  finalResult=text
 
 
-recognition.addEventListener( 'end', () =>
-{
+
+});
+
+
+recognition.addEventListener('end', () => {
   recognition.stop();
-  for ( let i = 0; i < finalResult.length; i++ )
-  {
-    if ( uppercaseAlphabets.includes( finalResult[ i ].toUpperCase() ) )
-    {
-      if ( cipher[ finalResult[ i ].toUpperCase() ] == undefined )
-      {
-        answer.push( " " );
-      } else
-      {
-        answer.push( cipher[ finalResult[ i ].toUpperCase() ] );
-      }
+
+  console.log(finalResult);
+
+  //handling vibration
+  for (let i = 0; i < finalResult.length; i++) {
+    if (uppercaseAlphabets.includes(finalResult[i].toUpperCase())) {
+        if (cipher[finalResult[i].toUpperCase()] == undefined) {
+          answer.push(" ")
+        }else{
+          answer.push(cipher[finalResult[i].toUpperCase()])
+        }
     }
   }
-  console.log( answer );
-  answer.forEach( ( e ) =>
-  {
-    var charArr = e.split( "" );
-    for ( let index = 0; index < charArr.length; index++ )
-    {
-      vibrateFun( charArr[ index ], index );
+  answer.forEach(function(item){
+    for (let i = 0; i < item.length; i++) {
+      const element = item[i];
+      if (element==".") {
+        console.log("dot");
+      }else if(element=="-"){
+        console.log("dash");
+      }else{
+        console.log("gap")
+      }
     }
-  } );
-} );
+  })
+});
 
-startBtn.addEventListener( 'click', () =>
-{
+startBtn.addEventListener('click', () => {
   recognition.start();
-} );
+});
 
 
 
-// Object.keys(cipher).forEach(function(key) {
-//   var value = cipher[key];
-//   console.log("Key:", key, "Value:", value);
-// });
+
+ 
